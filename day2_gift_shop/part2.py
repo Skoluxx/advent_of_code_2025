15dd9cbd06d0eef6dd9e48c030268945256696af
Hans Løkke <hansbloekke@gmail.com>
2025-12-02 21:45:34 +0100

    add function that return invalid ids from range

--- day2_gift_shop/part2.py
+++ day2_gift_shop/part2.py
@@ -16,9 +16,29 @@ def sum_ids(id_list):
 
     return total
 
+def invalid_ids(number_range):
+    ids = []
+    for number in range(number_range[0], number_range[1] + 1):
+        number_text = str(number)
+        for divisor in range(1, len(number_text)):
+            if len(number_text) % divisor == 0:
+                temp_text = number_text
+                cuts = len(number_text) // divisor
+                slices = []
+                for i in range(cuts):
+                    slices.append(temp_text[:divisor])
+                    temp_text = temp_text[divisor:]
+                if len(set(slices)) == 1 and number not in ids:
+                    ids.append(number)
+    return ids
+
 
 def main():
     number_ranges = get_ranges('test_input.md')
-    
+
+    ids = []
+    for number_range in number_ranges:
+        ids += invalid_ids(number_range)
+    print(ids)
 
 main()
