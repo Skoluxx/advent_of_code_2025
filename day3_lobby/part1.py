8073aa868203dd36e5f458ca7e73debf80a8aa78
Hans Løkke <hansbloekke@gmail.com>
2025-12-03 17:00:06 +0100

    creates func that finds highest joltage from bank

--- day3_lobby/part1.py
+++ day3_lobby/part1.py
@@ -5,9 +5,41 @@ def get_banks(filename):
             banks.append(line.strip())
     return banks
 
+def find_largest_joltage(bank):
+    number_list = list(bank)
+    
+    first_largest = 0
+    for idx in range(len(number_list) - 1):
+        if int(number_list[idx]) > first_largest:
+            first_largest = int(number_list[idx])
+
+    temp_number = int(number_list[0])
+    while temp_number != first_largest:
+         number_list.pop(0)
+         temp_number = int(number_list[0])
+        
+    if '9' in number_list[1:]:
+        second_largest = 9
+    else:
+        second_largest = 0
+        for idx in range(1, len(number_list)):
+            if int(number_list[idx]) > second_largest:
+                second_largest = int(number_list[idx])
+    
+    return int(f'{first_largest}{second_largest}')
+
+
 
 def main():
     banks = get_banks('test_input.md')
     print(banks)
+
+    largest_joltages = []
+    for bank in banks:
+        largest_joltages.append(find_largest_joltage(bank))
+
+    print(largest_joltages)
+
+
 
 main()
