a203833e0a9ccab42869bf879fbdb63bd8e67f54
Hans Løkke <hansbloekke@gmail.com>
2025-12-05 11:44:29 +0100

    change ingredient range func, bc it was way 2 slow

--- day5_cafeteria/part1.py
+++ day5_cafeteria/part1.py
@@ -1,19 +1,22 @@
-def get_ingredient_ids(filename):
-    ingredient_ids = set()
+def get_ingredient_id_ranges(filename):
+    ingredient_id_ranges = [] 
     with open(filename, 'r') as data:
         line = data.readline()
         while '-' in line:
             values = line.strip().split('-')
-            for number in range(int(values[0]), int(values[1]) + 1):
-                ingredient_ids.add(number)
+            ingredient_id_ranges.append([int(values[0]), int(values[1])])
             line = data.readline()
     return list(ingredient_ids)
 
 
 
 def main():
-    ingredient_ids = get_ingredient_ids('test_input.md')
+    ingredient_ids = get_ingredient_id_ranges('test_input.md')
     print(ingredient_ids)
+    
+    # fresh_ingredients = count_fresh_ingredients('puzzle_input.md', ingredient_ids)
+    # print(f'Fresh ingredients: {fresh_ingredients}')
+
 
 
 main()
