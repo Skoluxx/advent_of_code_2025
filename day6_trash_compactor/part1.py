e475e575c0d9ab2b76b1664c1221e1ba303da079
Hans Løkke <hansbloekke@gmail.com>
2025-12-06 21:19:47 +0100

    creates func that gets math probles from file

--- day6_trash_compactor/part1.py
+++ day6_trash_compactor/part1.py
@@ -1,7 +1,24 @@
 def get_problems(filename):
     problems = []
+
     with open(filename, 'r') as data:
         for line in data:
             values = line.strip().split()
-            for idx in range(values):
+
+            if not problems:
+                for item in values:
+                    problems.append([int(item)])
+            else:
+                for idx in range(len(values)):
+                    if values[idx].isdigit():
+                        values[idx] = int(values[idx])
+                    problems[idx].append(values[idx])
+    
+    print(problems)
+
+
+def main():
+    get_problems('test_input.md')
+
+main()
                 
