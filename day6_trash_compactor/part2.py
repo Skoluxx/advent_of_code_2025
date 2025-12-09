87b534be83e738fd334ae88b1a13b8990b6e4d0f
Hans Løkke <hansbloekke@gmail.com>
2025-12-09 12:39:24 +0100

    solves part 2 - holy ugly code, but it works

--- day6_trash_compactor/part2.py
+++ day6_trash_compactor/part2.py
@@ -1,14 +1,14 @@
 def add_numbers(numbers):
     sum = 0
     for number in numbers:
-        sum += number
+        sum += int(number)
 
     return sum
 
 def multiply_numbers(numbers):
-    product = numbers[0]
+    product = int(numbers[0])
     for number in numbers[1:]:
-        product *= number
+        product *= int(number)
 
     return product
 
@@ -53,15 +53,49 @@ def get_columns(filename):
 
     return columns
 
+def restrucutre_columns(columns):
+    return_columns = []
+
+    restructured_columns = []
+    for column in columns:
+        restructured_column = []
+        column = column[1:]
+        for number in column:
+            restructured_column.append(list(number))
+        
+        restructured_columns.append(restructured_column)
+
+    for idx in range(len(restructured_columns)):
+        restructured_columns[idx] = list(zip(*restructured_columns[idx]))
+
+    for idx in range(len(restructured_columns)):
+        for i in range(len(restructured_columns[idx])):
+            restructured_columns[idx][i] = list(restructured_columns[idx][i])
+
+    for column in restructured_columns:
+        for number in column:
+            while number[-1] == '0':
+                number.pop(-1)
+
+    for column in restructured_columns:
+        return_columns.append([''.join(column2) for column2 in column])
+        
+    for idx in range(len(columns)):
+        return_columns[idx].append(columns[idx][0])
+    
+    return return_columns
+        
 
 def main():
     grand_total = 0
-    columns = get_columns('test_input.md')
-    print(columns, '\n')
+    columns = get_columns('puzzle_input.md')
 
-    # for problem in problems:
-    #     grand_total += solve_problem(problem)
+    columns = restrucutre_columns(columns)
+    # print(columns, '\n')
+
+    for column in columns:
+        grand_total += solve_problem(column)
     
-    # print(f'grand total: {grand_total}')
+    print(f'grand total: {grand_total}')
 
 main()
