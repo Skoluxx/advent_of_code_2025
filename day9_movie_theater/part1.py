3a6d8e3124aee9f213057651096e5cec7cea90af
Hans Løkke <hansbloekke@gmail.com>
2025-12-16 17:19:40 +0100

    creates func to floor dimensions

--- day9_movie_theater/part1.py
+++ day9_movie_theater/part1.py
@@ -5,11 +5,24 @@ def get_positions(filename):
         for line in data:
             values = line.strip().split(',')
             positions.append([int(values[0]), int(values[1])])
-            
-    return positions
+
+    return sorted(positions)
+
+def get_dimensions(positions):
+    length = positions[-1][0]
+
+    width = 0
+    for position in positions:
+        if position[1] > width:
+            width = position[1]
+
+    return [length, width]
     
 def main():
     positions = get_positions('test_input.md')
     print(positions)
+    dimensions = get_dimensions(positions)
+    print(dimensions[0])
+    print(dimensions[1])
 
 main()
