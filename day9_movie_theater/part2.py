3e6e6da3febae5945577288c26c83fcf732e45e8
Hans Løkke <hansbloekke@gmail.com>
2025-12-19 17:51:40 +0100

    creates func to get green outer tile ranges

--- day9_movie_theater/part2.py
+++ day9_movie_theater/part2.py
@@ -8,9 +8,116 @@ def get_red_tiles(filename):
     
     return red_tiles
 
+def get_outer_green_tiles(red_tiles):
+    o_green_ranges = []
+
+    for idx in range(len(red_tiles) - 1):
+        x1 = min(red_tiles[idx][0], red_tiles[idx + 1][0])
+        x2 = max(red_tiles[idx][0], red_tiles[idx + 1][0])
+        y1 = min(red_tiles[idx][1], red_tiles[idx + 1][1])
+        y2 = max(red_tiles[idx][1], red_tiles[idx + 1][1])
+
+        if x1 == x2:
+            x = x1
+            y1 += 1
+            y2 -= 1
+
+            if y1 != y2:
+                o_green_ranges.append([[x], [y1, y2]])
+            else:
+                o_green_ranges.append([[x], [y1]])
+        
+        else:
+            y = y1
+            x1 += 1
+            x2 -= 1
+
+            if x1 != x2:
+                o_green_ranges.append([[x1, x2], [y]])
+            else:
+                o_green_ranges.append([[x1], [y]])
+
+    x1 = min(red_tiles[0][0], red_tiles[-1][0])
+    x2 = max(red_tiles[0][0], red_tiles[-1][0])
+    y1 = min(red_tiles[0][1], red_tiles[-1][1])
+    y2 = max(red_tiles[0][1], red_tiles[-1][1])
+
+    if x1 == x2:
+        x = x1
+        y1 += 1
+        y2 -= 1
+
+        if y1 != y2:
+            o_green_ranges.append([[x], [y1, y2]])
+        else:
+            o_green_ranges.append([[x], [y1]])
+    
+    else:
+        y = y1
+        x1 += 1
+        x2 -= 1
+
+        if x1 != x2:
+            o_green_ranges.append([[x1, x2 + 1], [y]])
+        else:
+            o_green_ranges.append([[y], [x1]])
+    return o_green_ranges
+
+def paint_grid(red_tiles, o_green_ranges):
+    length = 0
+    width = 0
+    for position in red_tiles:
+        if position[0] > length:
+            length = position[0]
+        if position[1] > width:
+            width = position[1]
+
+    dimensions = [length, width]
+
+    o_green = []
+    for o_range in o_green_ranges:
+        if len(o_range[0]) == 1 and len(o_range[1]) == 1:
+            o_green.append([o_range[0][0], o_range[1][0]])
+
+        elif len(o_range[0]) == 1:
+            x = o_range[0][0]
+
+            for y in range(o_range[1][0], o_range[1][1] + 1):
+                o_green.append([x, y])
+        else:
+            y = o_range[1][0]
+
+            for x in range(o_range[0][0], o_range[0][1] + 1):
+                o_green.append([x, y])
+    
+    print('Painting grid:')
+    for y in range(dimensions[1] + 2):
+        row = []
+        for x in range(dimensions[0] + 3):
+            if [x, y] in red_tiles:
+                row.append('#')
+            elif [x, y] in o_green:
+                row.append('X')
+            else:
+                row.append('.')
+
+        print(''.join(row))
 
 def main():
+    print('Getting red tiles')
     red_tiles = get_red_tiles('test_input.md')
-    print(red_tiles)
+    print('Got red tiles')
+    print('len:', len(red_tiles))
+    print('Red tiles:', red_tiles, '\n')
+
+    print('Getting outer green tile ranges')
+    o_green_ranges = get_outer_green_tiles(red_tiles)
+    print('Got outer green tile ranges')
+    print('len:', len(o_green_ranges))
+    print('Outer green tile ranges:', o_green_ranges, '\n')
+
+    paint_grid(red_tiles, o_green_ranges)
+
+
 
 main()
